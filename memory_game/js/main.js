06976db64919c160736513453cdf528e05f7df32
nasir95 <nas.nooruddin@gmail.com>
2019-10-29 10:17:03 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,23 @@
 const cards = ["queen","queen","king","king"];
 const cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
+
+function checkforMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+
+	function flipCard(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
 if(cardsInPlay.length === 2){
-	if (cardsInPlay[0] === cardsInPlay[2]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again");
+	 checkforMatch();
 	}
-} 
+
+	}
+
+
+flipCard(0);
+flipCard(2);
