d00a59465677a5163315d1867318398bbc3897d6
nasir95 <nas.nooruddin@gmail.com>
2019-10-29 12:19:36 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-const cards = ["queen","queen","king","king"];
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage:"images/queen-of-hearts.png",
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage:"images/queen-of-diamonds.png",
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage:"images/king-of-hearts.png",
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage:"images/king-of-diamonds.png",
+},
+];
 const cardsInPlay = [];
 
 function checkforMatch(){
@@ -10,14 +31,22 @@ function checkforMatch(){
 }
 
 	function flipCard(cardId){
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 if(cardsInPlay.length === 2){
+	if (cardsInPlay === cardsInPlay) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
 	 checkforMatch();
 	}
+}
 
-	}
+	
 
 
 flipCard(0);
-flipCard(2);
+flipCard(1);
